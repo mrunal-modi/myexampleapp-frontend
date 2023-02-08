511fd82431ebcbb459c42cb9289c634e54a3bba3
mrunal-modi <mrunal.modi@gmail.com>
2023-02-08 11:03:14 +1100

    crud

---
+++ src/components/crud/crud-item.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+
+import "./crud-item.scss";
+
+const CrudItem = ({ title, isDone, onDelete, onChange }) => {
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue ] = useState(title);
+
+  const handleChange = (name, value) => {
+    onChange(name, value);
+  };
+
+  const handleEnterKey = async (e) => {
+    if (e.key === 'Enter') {
+      console.log('do validate');
+      handleChange('todo', value);
+      setIsEdit(false);
+    }
+  }
+
+  return (
+    <div className="crud-item card d-flex flex-row margin-bottom-20">
+      <input
+        type="checkbox"
+        className="margin-right-10"
+        checked={isDone}
+        onChange={() => handleChange("isDone", !isDone)}
+      />
+      {
+        isEdit && 
+        <input 
+        onChange={(e)=> { setValue(e.target.value)}} value={value}
+        onKeyUp={handleEnterKey}
+        type="text" 
+        placeholder="Enter new item" 
+        className="form-control margin-bottom-20"/>
+      }
+      {
+        !isEdit &&
+        <h5>{title}</h5>
+      }
+      
+      <UncontrolledDropdown className="ml-auto">
+        <DropdownToggle nav caret>
+          <i className="fa fa-ellipsis-v"></i>
+        </DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem onClick={() => {setIsEdit(!isEdit)}}>Edit</DropdownItem>
+          <DropdownItem onClick={onDelete}>Delete</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </div>
+  );
+};
+
+export default CrudItem;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,44 @@
 import "./home.scss";
+import React, { useState } from "react";
 import Form from "../../components/form/form";
 import Title from "../../components/title/title";
-import {_create} from "../../services/services";
+import { _create } from "../../services/services";
+import CrudItem from "../../components/crud/crud-item";
 
 const Home = () => {
-    
-    const handleSubmit = (data) => {
-        _create(data.email);
+
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState();
+
+    // const handleSubmit = (data) => {
+    //     _create(data.email);
+    // }
+
+    const handleSubmit = async (data) => {
+        try {
+            const res = await _create(data.email);;
+            if (res.status === 200) {
+                setSuccess(true);
+            }
+        } catch (err) {
+            setError(err?.response?.data?.error);
+            console.log(err.response);
+        }
     }
+
     return (
         <div className="home-page">
             <div className="column">
-                <Title title="Create"/>
+
+                <Title title="Create" />
+
+                {success && <div className="alert alert-success col-12">
+                    Successfully submitted!
+                </div>}
+                {error && <div className="alert alert-danger col-12">
+                    {error}
+                </div>}
+
                 <Form className="post-form"
                     formInputs={
                         [
@@ -28,8 +55,9 @@ const Home = () => {
                 />
             </div>
             <div className="column">
-            <Title title="Read | Update | Delete"/>
+                <Title title="Read | Update | Delete" />
 
+                <CrudItem/>
             </div>
 
         </div>
